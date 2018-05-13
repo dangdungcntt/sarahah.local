1a34ae40d318a4edb2a613e43ba164d1639532d1
QuanLuong <dinhquan15797@gmail.com>
2018-05-13 23:03:52 +0700

    Update share.php

--- action/share.php
+++ action/share.php
@@ -3,7 +3,7 @@ echo "<img src='../img/share/Message-".$image.".png'>";
  ?>
 <meta property="og:title" content="Nhắn tin ẩn danh" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://chát.vn" />
+<meta property="og:url" content="https://sarahah.devv" />
 <meta property="og:image" content="../img/share/Message-<?php echo $image; ?>.png" />
 <meta property="og:site_name" content="Sarahah" />
 <meta property="fb:app_id" content="2059815907622589" />
