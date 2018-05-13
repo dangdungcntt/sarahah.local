c802f8af58fd20fdc43f87c6d30f73105a737ac7
QuanLuong <dinhquan15797@gmail.com>
2018-05-13 21:42:08 +0700

    last

--- action/share.php
+++ action/share.php
@@ -2,10 +2,10 @@
 echo "<img src='".$image."'>";
  ?>
 <img src="'$image'">
-<meta property="og:title" content="Xu hướng công nghệ" />
+<meta property="og:title" content="Nhắn tin ẩn danh" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://socolaworld.ga" />
 <meta property="og:image" content="/img/share/Message-<?php echo $image; ?>.png" />
 <meta property="og:site_name" content="Honest" />
 <meta property="fb:app_id" content="580972292069229" />
-<meta property="og:description" content="Ahihi." />
+<meta property="og:description" content="Nhắn tin." />
