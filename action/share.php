68bc20b5a77dc733c19a49bbc526739a370aef5d
QuanLuong <dinhquan15797@gmail.com>
2018-05-13 22:54:35 +0700

    last 5

--- action/share.php
+++ action/share.php
@@ -3,8 +3,8 @@ echo "<img src='../img/share/Message-".$image.".png'>";
  ?>
 <meta property="og:title" content="Nhắn tin ẩn danh" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://sarahah.devv/" />
+<meta property="og:url" content="https://chát.vn/" />
 <meta property="og:image" content="../img/share/Message-<?php echo $image; ?>.png" />
-<meta property="og:site_name" content="Honest" />
+<meta property="og:site_name" content="Sarahah" />
 <meta property="fb:app_id" content="2059815907622589" />
 <meta property="og:description" content="Nhắn tin." />
